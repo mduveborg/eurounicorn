8fe4b2140e174519620157ba68cf942b06cde0e9
Daniel Werthén <danielwerthen@gmail.com>
2014-02-21 20:28:43 +0100

    Added backend with nancy framework

---
+++ backend/EurounicornAPI/EurounicornAPI/HelloModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EurounicornAPI
+{
+    public class HelloModule : Nancy.NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
