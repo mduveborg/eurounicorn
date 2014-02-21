c5f122215f000d2dd844fd1fb9094effeeadeb7c
Daniel Werthén <danielwerthen@gmail.com>
2014-02-21 21:32:35 +0100

    Coupled backend with frontend

---
+++ backend/EurounicornAPI/EurounicornAPI/DistModule.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EurounicornAPI
+{
+    public class DistModule : NancyModule
+    {
+        public DistModule()
+        {
+            Get["/{path*}"] = _ =>
+            {
+                string path = _.path;
+                
+                return Response.AsFile(string.Format("dist/{0}", path));
+            };
+            Get["/"] = _ =>
+            {
+                string path = _.path;
+
+                return Response.AsFile("dist/index.html");
+            };
+        }
+    }
+}
