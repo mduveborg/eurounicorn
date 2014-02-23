4f5fe4b4bb5c81451ac8fa3f1dabde17ab5203c0
Daniel Werthén <danielwerthen@gmail.com>
2014-02-23 14:18:37 +0100

    Passthrough images too

--- frontend/app/scripts/apiMock.js
+++ frontend/app/scripts/apiMock.js
@@ -6,7 +6,10 @@ angular.module('frontendAppDev', ['frontendApp', 'ngMockE2E'])
 		function regEsc(str) {
 			return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\-\\\^\$\|]/g, '\\$&');
 		}
-		// Pass through request for views
+		// Pass through request for views and images
 		$httpBackend.whenGET( new RegExp( regEsc( 'views/' ) ) ).passThrough();
+		$httpBackend.whenGET( new RegExp( regEsc( 'images/' ) ) ).passThrough();
+
+		//override this route with a response
 		$httpBackend.whenPOST('/api/login').respond({ token: 'dev' });
 	});
