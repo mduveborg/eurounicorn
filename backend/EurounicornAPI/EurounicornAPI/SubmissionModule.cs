67aa93f99910b630bb57f2a19d2d28bb265db8eb
Daniel Werthén <danielwerthen@gmail.com>
2014-03-03 02:52:24 +0100

    Final

--- backend/EurounicornAPI/EurounicornAPI/SubmissionModule.cs
+++ backend/EurounicornAPI/EurounicornAPI/SubmissionModule.cs
@@ -143,7 +143,7 @@ namespace EurounicornAPI
 
         private string Embedd(int trackId, string secret)
         {
-            return @"<iframe width=""100%"" height=""450"" scrolling=""no"" frameborder=""no"" src=""https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + 
+            return @"<iframe width=""100%"" height=""300"" scrolling=""no"" frameborder=""no"" src=""https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + 
                 trackId + "%3Fsecret_token%3D" + secret + @"&amp;auto_play=false&amp;hide_related=false&amp;visual=true""></iframe>";
         }
     }
