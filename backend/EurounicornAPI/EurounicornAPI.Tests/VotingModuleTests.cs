1d18769b446f853c1d0053c87ac357ecafbd74c5
Alex <alex@raketmaskin.com>
2014-03-22 14:37:40 +0100

    Added VotingModule

---
+++ backend/EurounicornAPI/EurounicornAPI.Tests/VotingModuleTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Testing;
+
+namespace EurounicornAPI.Tests
+{
+    [TestClass]
+    public class VotingModuleTests
+    {
+        [TestMethod]
+        public void TestGettingVotes()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            // When
+            var result = browser.Post("/api/vote", with =>
+            {
+                with.HttpRequest();
+            });
+
+            // Then
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+    }
+}
---
+++ backend/EurounicornAPI/EurounicornAPI/VotingModule.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using EurounicornAPI.CouchDB;
+using EurounicornAPI.DtoObjects;
+using EurounicornAPI.Voting;
+using EurounicornAPI.Voting.Entities;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace EurounicornAPI
+{
+    // Make a lazy instantiated CouchDBService
+    public class VotingModule : NancyModule
+    {
+        public VotingModule() : base("api/vote")
+        {
+            var _couchDbService = new CouchDBService(); //make lazy
+
+            this.Post["/", true] = (_, cancel) =>
+            {
+                return Task.Run<dynamic>(() =>
+                {
+                    var votesToPost = this.Bind<Vote>();
+                    var userName = Context.CurrentUser.UserName;
+                    var votingService = new VotingService(_couchDbService);
+
+                    votingService.CastVote(userName, votesToPost.TrackId, votesToPost.Points);
+
+                    return HttpStatusCode.OK;
+                });
+            };
+            
+            this.Get["/track", true] = (_, cancel) =>
+            {
+                return Task.Run<dynamic>(() =>
+                {
+                    var trackId = this.Bind<TrackDto>().Id;
+                    var votingService = new VotingService(_couchDbService);
+
+                    var votesForTrack = votingService.GetVotesForTrack(trackId);
+
+                    return Response.AsJson(votesForTrack, HttpStatusCode.OK);
+                });
+            };
+
+            this.Get["/turnout", true] = (_, cancel) =>
+            {
+                return Task.Run<dynamic>(() =>
+                {
+                    var votingService = new VotingService(_couchDbService);
+
+                    var turnout = votingService.GetVoterTurnout();
+
+                    return Response.AsJson(turnout, HttpStatusCode.OK);
+                });
+            };
+
+            this.Get["/user", true] = (_, cancel) =>
+            {
+                return Task.Run<dynamic>(() =>
+                {
+                    var userName = Context.CurrentUser.UserName;
+                    var votingService = new VotingService(_couchDbService);
+
+                    var votesForUser = votingService.GetVotesForUser(userName);
+
+                    return Response.AsJson(votesForUser, HttpStatusCode.OK);
+                });
+            };
+        }
+    }
+}
